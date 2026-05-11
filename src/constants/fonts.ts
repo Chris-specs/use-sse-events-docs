1031b07bca739604ed249218bc11ba55b0285ef5
Christian Sanchez <christian.s.molina@gmail.com>
2026-05-11 15:51:05 -0600

    feat: Add shadcn

---
+++ src/constants/fonts.ts
@@ -0,0 +1,9 @@
+import { Geist_Mono, Syne } from 'next/font/google'
+
+// export const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
+export const inter = Syne({ subsets: ['latin'], variable: '--font-sans' })
+
+export const geistMono = Geist_Mono({
+    subsets: ['latin'],
+    variable: '--font-geist-mono'
+})
